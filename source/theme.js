bcbb89fa724275ee75d801abb178e9b199f6299f
awitherow <au.witherow@gmail.com>
2017-07-14 17:53:43 +0200

    feat(intro): add basic getting started page

--- source/theme.js
+++ source/theme.js
@@ -1,14 +1,16 @@
-import { Dimensions } from "react-native"; 
+import {Dimensions} from "react-native";
 
-const { height, width } = Dimensions.get('window');
+const {height, width} = Dimensions.get("window");
 
 export default {
-    colors: {
-	lightBlue: "rgb(11,157,253)",
-	darkBlue: "#053a59",
-    },
-    metrics: {
-	width,
-	height,
-    }
-}
+  colors: {
+    lightBlue: "rgb(11,157,253)",
+    darkBlue: "#053a59",
+    opaqueDarkBlue: opacity => `rgba(5, 58, 89, ${opacity})`,
+    opaqueLightBlue: opacity => `rgba(11, 157, 253, ${opacity})`,
+  },
+  metrics: {
+    width,
+    height,
+  },
+};

c6a32e9f1206db42c03c0b10a5f6afee069ef66e
awitherow <au.witherow@gmail.com>
2017-07-19 22:16:13 +0200

    chore(services): add mailchimp subscribe function

---
+++ source/services/mailchimp.js
@@ -0,0 +1,34 @@
+import { mailchimp } from "../config.js";
+const { instance, list, key } = mailchimp;
+
+export function subscribe(email) {
+  return new Promise(function(resolve, reject) {
+    request
+      .post(
+        "https://" +
+          instance +
+          ".api.mailchimp.com/3.0/lists/" +
+          list +
+          "/members/"
+      )
+      .set("Content-Type", "application/json;charset=utf-8")
+      .set(
+        "Authorization",
+        "Basic " + new Buffer("any:" + keymailchimpApiKey).toString("base64")
+      )
+      .send({
+        email_address: email,
+        status: "subscribed"
+      })
+      .end(function(err, response) {
+        if (
+          response.status < 300 ||
+          (response.status === 400 && response.body.title === "Member Exists")
+        ) {
+          resolve(true, null);
+        } else {
+          reject(false, err);
+        }
+      });
+  });
+}
